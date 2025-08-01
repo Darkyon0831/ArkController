a812bc1c50dd80da54231f8c1110d82b83b82a6c
Darkyon0831 <adrian.rondahl@live.se>
2025-08-01 07:42:32 +0000

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoginPage />
+  )
+}
+
+function LoginPage() {
+  return (
+    <>
+      <div className="login-page">
+        <div className="login-window">
+          <h2>Login</h2>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button>Login</button>
+        </div>
+      </div>
+    </>
   );
 }
 
